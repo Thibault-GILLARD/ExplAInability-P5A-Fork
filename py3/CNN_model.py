4f4ce4155a038fe67d9a96af74ea9e0c857b490b
Thibault-GILLARD <thibault.gillard@epfedu.fr>
2023-12-21 19:43:22 +0100

    clean CNN_model.py

--- py3/CNN_model.py
+++ py3/CNN_model.py
@@ -55,30 +55,4 @@ class Cplx_CustomCNN_1D(layers.Layer):
     def summary(self):
         self.model.summary()
         
-        
-from efficientnet.keras import EfficientNetB0
-
-class New_CustomCNN1D(tf.keras.Model):
-    def __init__(self, input_shape=(73, 10), num_classes=8):
-        super(New_CustomCNN1D, self).__init__()
-
-        self.model = tf.keras.Sequential()
-
-        self.model.add(layers.Conv1D(64, kernel_size=2, activation='relu', input_shape=input_shape))
-        self.model.add(layers.MaxPooling1D(pool_size=2))
-        self.model.add(layers.Conv1D(128, kernel_size=2, activation='relu'))
-        self.model.add(layers.MaxPooling1D(pool_size=2))
-        self.model.add(layers.Conv1D(256, kernel_size=2, activation='relu'))
-        self.model.add(layers.MaxPooling1D(pool_size=2))
-        self.model.add(layers.GlobalAveragePooling1D())
-        self.model.add(layers.Dense(512, activation='relu'))
-        self.model.add(layers.BatchNormalization())
-        self.model.add(layers.GlobalAveragePooling1D())
-    def call(self, inputs):
-        return self.model(inputs)
-
-    def compile_model(self):
-        self.model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
-
-    def summary(self):
-        self.model.summary()
+  
